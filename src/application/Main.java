a33c19b3a49ff8ef783d282ad896923cabfd4a76
WeitingWang0704 <wang.weiti@northeastern.edu>
2024-11-17 15:43:17 -0500

    Initial implementation of Pomodoro Timer UI

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,38 +3,48 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.Group;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
- 
-	
-
-
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		Group root = new Group();
-		Text text = new Text();
-		text.setText("Hello World");
-		
-		primaryStage.setWidth(420);
-		primaryStage.setHeight(420);
-		text.setX((primaryStage.getWidth() - text.getLayoutBounds().getWidth()) / 2);
-		text.setY((primaryStage.getWidth() - text.getLayoutBounds().getWidth()) / 2);
-		root.getChildren().add(text);
-		
-		Scene scene = new Scene(root);
-		
-		primaryStage.setTitle("Stage Demo Program");
-		
-		// Disable Resize
-		primaryStage.setResizable(false);
+    @Override
+    public void start(Stage primaryStage) {
+        
+    	VBox root = new VBox(15); 
+        root.setStyle("-fx-padding: 20; -fx-alignment: center; -fx-background-color: #f4f4f4;");
 
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
+        Text timerText = new Text("25:00");
+        timerText.setFont(Font.font("Arial", 50));
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+        Button pomodoroButton = new Button("Pomodoro");
+        Button breakButton = new Button("Break");
+        pomodoroButton.setStyle("-fx-background-color: #d0c1ff; -fx-font-size: 14;");
+        breakButton.setStyle("-fx-background-color: #d0c1ff; -fx-font-size: 14;");
+
+        ComboBox<String> taskTypeComboBox = new ComboBox<>();
+        taskTypeComboBox.getItems().addAll("Study", "Work", "Exercise");
+        taskTypeComboBox.setPromptText("Task类型");
+
+        TextField taskInput = new TextField();
+        taskInput.setPromptText("Task");
+
+        Button startButton = new Button("START");
+        startButton.setStyle("-fx-background-color: #d0c1ff; -fx-font-size: 18;");
+
+        root.getChildren().addAll(pomodoroButton, breakButton, timerText, taskTypeComboBox, taskInput, startButton);
+
+        Scene scene = new Scene(root, 400, 300);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Pomodoro Timer");
+        primaryStage.setResizable(false); 
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
